287f1bb76cd2d84be8c99567e34bfff4308ea9c9
Richane Ramsoedit <richane_ramsoedit@hotmail.com>
2023-05-24 20:18:06 +0200

    Added HANStack

---
+++ icss2022-sep-main/icss2022-sep-main/startcode/src/main/java/nl/han/ica/datastructures/HANStack.java
@@ -0,0 +1,26 @@
+package nl.han.ica.datastructures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HANStack<T> implements IHANStack<T>{
+    private List<T> stack;
+
+    public HANStack (){
+        stack = new ArrayList<>();
+    }
+    @Override
+    public void push(T value) {
+        stack.add(value);
+    }
+
+    @Override
+    public T pop() {
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
+}
