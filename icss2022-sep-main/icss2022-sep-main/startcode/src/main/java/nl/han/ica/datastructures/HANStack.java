24397c235a16a3c430c48913d11b83351d155fc2
Richane Ramsoedit <richane_ramsoedit@hotmail.com>
2023-05-29 15:40:42 +0200

    Update HANStack.java

--- icss2022-sep-main/icss2022-sep-main/startcode/src/main/java/nl/han/ica/datastructures/HANStack.java
+++ icss2022-sep-main/icss2022-sep-main/startcode/src/main/java/nl/han/ica/datastructures/HANStack.java
@@ -7,7 +7,7 @@ public class HANStack<T> implements IHANStack<T>{
     private HANLinkedList<T> stack;
 
     public HANStack (){
-        stack = new HANLinkedList();
+        stack = new HANLinkedList<>();
     }
     @Override
     public void push(T value) {
@@ -16,11 +16,20 @@ public class HANStack<T> implements IHANStack<T>{
 
     @Override
     public T pop() {
-        return null;
+        if(isEmpty()){
+            throw new IllegalStateException("Stack is empty");
+        }
+        T value = stack.getFirst();
+        stack.removeFirst();
+        return value;
     }
 
     @Override
     public T peek() {
         return null;
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
     }
 }
