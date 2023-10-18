86f486a0eacc32ca1fc8544329214678633864f3
Richane Ramsoedit <richane_ramsoedit@hotmail.com>
2023-10-18 17:11:15 +0200

    findAllVariables toegevoegd

--- icss2022-sep-main/icss2022-sep-main/startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ icss2022-sep-main/icss2022-sep-main/startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -1,7 +1,8 @@
 package nl.han.ica.icss.checker;
 
+import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
-import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.*;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
@@ -13,7 +14,34 @@ public class Checker {
     private IHANLinkedList<HashMap<String, ExpressionType>> variableTypes;
 
     public void check(AST ast) {
-        // variableTypes = new HANLinkedList<>();
+        variableTypes = new HANLinkedList<>();
+        variableTypes.addFirst(new HashMap<>());
+
+
+
+    }
+
+    private void checkUndefinedVariables(ASTNode toBeChecked) {
+        if(toBeChecked.getChildren().size() != 1){
+            if(toBeChecked instanceof VariableReference){
+                // if(!variableTypes.contains(toBeChecked)){
+                //     // throw new Exception("Variable not defined");
+                // }
+            }
+        }
+    }
+
+
+    private void findAllVariables(ASTNode toBeFound){
+        if(toBeFound instanceof VariableAssignment){
+            String name = ((VariableAssignment) toBeFound).name.name;
+            ExpressionType expressionType = resolveExpressionType(((VariableAssignment) toBeFound).expression);
+            variableTypes.getFirst().put(name, expressionType);
+        }
+        toBeFound.getChildren().forEach(this::findAllVariables);
+    }
+
+    private ExpressionType resolveExpressionType(Expression expression){
 
     }
 
