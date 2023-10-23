fd11b2e709495fd6a7214e34329e86f7cedbbdad
Richane Ramsoedit <richane_ramsoedit@hotmail.com>
2023-10-23 21:05:35 +0200

    Update CheckerTest.java

--- icss2022-sep-main/icss2022-sep-main/startcode/src/test/java/nl/han/ica/icss/checker/CheckerTest.java
+++ icss2022-sep-main/icss2022-sep-main/startcode/src/test/java/nl/han/ica/icss/checker/CheckerTest.java
@@ -1,7 +1,10 @@
 package nl.han.ica.icss.checker;
 
+import nl.han.ica.icss.Pipeline;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
+import org.junit.jupiter.api.Test;
+
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -12,10 +15,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CheckerTest {
     private String readFile(String resource) throws IOException {
-        File file = new File(String.format("./src/resources/%s", resource));
+        File file = new File(String.format("./src/main/resources/%s", resource));
         InputStream inputStream = new FileInputStream(file);
         CharStream charStream = CharStreams.fromStream(inputStream);
         return charStream.toString().replaceAll("\r\n", "\n");
+    }
+
+    @Test
+    void testCheckCH01() throws IOException {
+        Pipeline pipeline = new Pipeline();
+        pipeline.parseString(this.readFile("CH01testbestand.icss"));
+        boolean success = pipeline.check();
+        assertTrue(success);
     }
 
 }
