8885b56457e16802c5a0489572e831dbd5ed4282
Richane Ramsoedit <richane_ramsoedit@hotmail.com>
2023-10-24 19:25:04 +0200

    Update CheckerTest.java

--- icss2022-sep-main/icss2022-sep-main/startcode/src/test/java/nl/han/ica/icss/checker/CheckerTest.java
+++ icss2022-sep-main/icss2022-sep-main/startcode/src/test/java/nl/han/ica/icss/checker/CheckerTest.java
@@ -3,22 +3,32 @@ package nl.han.ica.icss.checker;
 import nl.han.ica.icss.Pipeline;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CheckerTest {
+    private final ByteArrayOutputStream outputContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errorContent = new ByteArrayOutputStream();
+    private final PrintStream originalOutput = System.out;
+    private final PrintStream originalError = System.err;
+
+
     private String readFile(String resource) throws IOException {
         File file = new File(String.format("./src/main/resources/%s", resource));
         InputStream inputStream = new FileInputStream(file);
         CharStream charStream = CharStreams.fromStream(inputStream);
         return charStream.toString().replaceAll("\r\n", "\n");
+    }
+
+    @BeforeEach
+    public void setUpStreams(){
+        System.setOut(new PrintStream(outputContent));
+        System.setErr(new PrintStream(errorContent));
     }
 
     /* First all the level files, which have correct semantics, are tested
@@ -64,6 +74,8 @@ class CheckerTest {
         pipeline.parseString(this.readFile("CH01testbestand.icss"));
         boolean success = pipeline.check();
         assertFalse(success, "The checker should fail on CH01testbestand.icss");
+
+        String consoleOutput = outContent.toString
     }
 
 
