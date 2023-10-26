26d107d7356a20f8b2e6ea5756bc3186c5112295
Richane Ramsoedit <richane_ramsoedit@hotmail.com>
2023-10-26 19:23:49 +0200

    Update Generator.java

--- icss2022-sep-main/icss2022-sep-main/startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ icss2022-sep-main/icss2022-sep-main/startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -1,15 +1,36 @@
 package nl.han.ica.icss.generator;
 
 
+import nl.han.ica.datastructures.HANLinkedList;
+import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.ASTNode;
+import nl.han.ica.icss.ast.Expression;
+import nl.han.ica.icss.ast.Selector;
+
+import java.util.HashMap;
 
 public class Generator {
 
+	private StringBuilder stringBuilder;
+	private IHANLinkedList<HashMap<String, Expression>> variables;
+
 	public String generate(AST ast) {
-        return "";
+        variables = new HANLinkedList<>();
+		variables.addFirst(new HashMap<>());
+		stringBuilder = new StringBuilder();
 
+		generateResult(ast.root);
 
+		return stringBuilder.toString() + "}";
 	}
 
+	private void generateResult(ASTNode node) {
+		if (node instanceof Selector) {
+			if (stringBuilder.toString().endsWith(";\n")) {
+				stringBuilder.append("}\n\n");
+			}
+		}
+	}
 	
 }
