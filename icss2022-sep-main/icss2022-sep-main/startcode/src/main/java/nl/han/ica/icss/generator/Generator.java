471d324870e25aaee1edfeeac4a5a3791f807db5
Richane Ramsoedit <richane_ramsoedit@hotmail.com>
2023-10-26 19:50:13 +0200

    Update Generator.java

--- icss2022-sep-main/icss2022-sep-main/startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ icss2022-sep-main/icss2022-sep-main/startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -3,11 +3,12 @@ package nl.han.ica.icss.generator;
 
 import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
-import nl.han.ica.icss.ast.AST;
-import nl.han.ica.icss.ast.ASTNode;
-import nl.han.ica.icss.ast.Expression;
-import nl.han.ica.icss.ast.Selector;
+import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.selectors.ClassSelector;
+import nl.han.ica.icss.ast.selectors.IdSelector;
+import nl.han.ica.icss.ast.selectors.TagSelector;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Generator {
@@ -29,6 +30,39 @@ public class Generator {
 		if (node instanceof Selector) {
 			if (stringBuilder.toString().endsWith(";\n")) {
 				stringBuilder.append("}\n\n");
+			}
+			generateSelectorResult((Selector) node);
+		}
+		if (node instanceof Declaration) {
+			if (stringBuilder.toString().endsWith(" ")) {
+				stringBuilder.append("{\n");
+			}
+			generateBodyResult(node);
+		}
+		node.getChildren().forEach(this::generateResult);
+	}
+
+	private void generateSelectorResult(Selector selector) {
+		if (selector instanceof ClassSelector) {
+			stringBuilder.append(((ClassSelector) selector).cls);
+		} else if (selector instanceof IdSelector) {
+			stringBuilder.append(((IdSelector) selector).id);
+		} else if (selector instanceof TagSelector) {
+			stringBuilder.append(((TagSelector) selector).tag);
+		}
+		stringBuilder.append(" ");
+	}
+
+	private void generateBodyResult(ASTNode node) {
+		if (node instanceof Declaration) {
+			generateDeclarationResult((Declaration) node);
+		}
+	}
+
+	private void generateDeclarationResult(ArrayList<ASTNode> nodes) {
+		for (ASTNode node : nodes) {
+			if (node instanceof PropertyName) {
+				
 			}
 		}
 	}
